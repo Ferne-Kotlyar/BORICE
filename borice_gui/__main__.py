dd45ee562241ed7580fa78b1e248ea7ce7b6fd77
Adrien GIVRY <contact@adrien-givry.com>
2022-06-23 11:02:57 -0400

    Removing TestWindow

--- borice_gui/__main__.py
+++ borice_gui/__main__.py
@@ -12,49 +12,5 @@ def main():
 	window.show()
 	sys.exit(app.exec_())
 
-class TestWindow(QMainWindow):
-	def __init__(self, parent, app):
-		super(TestWindow, self).__init__(parent)
-		self.parent = parent
-		self.app = app
-
-		self.resize(500,350)
-		self.setWindowTitle("BORICE")
-
-		self.buildUI()
-
-	def buildUI(self):
-		"""
-		Creates the layout and all widgets for the MainWindow.
-		"""
-		# Central Widget
-		centralWidget = QWidget(self)
-		centralLayout = QVBoxLayout()
-		centralLayout.setAlignment(QtCore.Qt.AlignCenter)
-		centralWidget.setLayout(centralLayout)
-		self.setCentralWidget(centralWidget)
-
-		tabs = QTabWidget()
-
-		# General Settings Box
-		settingsBox = QWidget()
-		settingsBoxForm = QFormLayout(settingsBox)
-		settingsBoxForm.setFormAlignment(Qt.AlignmentFlag.AlignCenter)
-		settingsBoxForm.setFieldGrowthPolicy(QFormLayout.FieldGrowthPolicy.ExpandingFieldsGrow)
-		settingsBox.setLayout(settingsBoxForm)
-
-		tabs.addTab(settingsBox, "Main")
-
-		# Foo
-		foo = QCheckBox()
-		foo.setChecked(False)
-		foo.setMaximumWidth(250)
-		settingsBoxForm.addRow("Foo this is the way lol this is very long string!!!!!", foo)
-
-		
-
-		# Populate the central layout
-		centralLayout.addWidget(tabs)
-
 if __name__ == '__main__': 
 	main()
